130a477dfac2216d30407969509e723b81be85c3
linper <llinas7879@gmail.com>
2020-06-06 09:16:37 +0300

    added cmd history (on linux machine)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
 import Parser as p
+import readline
 import exec
 
 if __name__ == '__main__':
     exec.__collect_names()
-    print("pCal 1.0")
+    print("PCal 1.0")
     while True:
         line = input()
         p.parse(line, False)
